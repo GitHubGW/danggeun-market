ea32eb100b8528934026bc9c5a857cc0ff9d259e
GW <kowonp@gmail.com>
2022-04-24 00:01:04 +0900

    build(package): set fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import "../styles/globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import type { AppProps } from "next/app";
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
