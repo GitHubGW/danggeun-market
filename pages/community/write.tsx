ac1b00fc77c892deb670e127f1e51b90885d52ca
GW <kowonp@gmail.com>
2022-04-25 17:36:06 +0900

    style(write.tsx): community write

---
+++ pages/community/write.tsx
@@ -0,0 +1,23 @@
+import Button from "../../components/button";
+import { RiImageAddFill } from "react-icons/ri";
+
+const CommunityWrite = () => {
+  return (
+    <div className="wrapper">
+      <div className="content-sub">
+        <form className=" flex flex-col">
+          <label className="transition-all rounded-lg border-dashed border-2 text-gray-200 hover:border-orange-400 hover:text-orange-400 flex justify-center items-center h-[500px] cursor-pointer">
+            <RiImageAddFill className="text-[40px]" />
+            <input type="file" accept="image/*" className="hidden" />
+          </label>
+          <label className="mt-5 mb-2">
+            <textarea rows={8} maxLength={400} placeholder="근처의 이웃에게 질문하거나 이야기를 해보세요." className="resize-none input ring-normal" />
+          </label>
+          <Button type="submit" text="동네생활 글쓰기" size="w-full" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CommunityWrite;
