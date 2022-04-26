ed94918bf50e2370cf6b391b692c88d64ad55733
GW <kowonp@gmail.com>
2022-04-27 00:16:22 +0900

    style(edit.tsx): profile edit

---
+++ pages/profile/edit.tsx
@@ -0,0 +1,39 @@
+import Button from "../../components/button";
+import LabelTitle from "../../components/label-title";
+
+const ProfileEdit = () => {
+  return (
+    <div className="wrapper wrapper-content">
+      <div className="content-sub">
+        <form className="flex flex-col mt-10">
+          <label className="transition-all rounded-full flex justify-center items-center cursor-pointer">
+            <img
+              src={true ? "/images/basic_user.png" : "https://newsimg.sedaily.com/2022/02/21/26288ZY0E1_3.jpg"}
+              alt=""
+              className="border border-gray-200 rounded-full w-64 h-64"
+            />
+            <input type="file" required accept="image/*" className="hidden" />
+          </label>
+          <label className="mt-5">
+            <LabelTitle text="유저 이름" />
+            <input type="text" placeholder="유저 이름을 입력해주세요." className="input ring-normal" />
+          </label>
+          <label className="mt-5">
+            <LabelTitle text="이메일" />
+            <input type="email" placeholder="이메일을 입력해주세요." required className="input ring-normal" />
+          </label>
+          <label className="mt-5 mb-5">
+            <LabelTitle text="휴대폰 번호" />
+            <div className="relative">
+              <span className="absolute top-1/2 left-2.5 -translate-y-1/2 text-gray-300 select-none">+82</span>
+              <input type="number" placeholder="휴대폰 번호를 입력해주세요." required className="input-space pl-12 pr-2 ring-normal" />
+            </div>
+          </label>
+          <Button type="submit" text="프로필 업데이트" size="w-full" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileEdit;
