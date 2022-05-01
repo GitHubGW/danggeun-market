cd3024b68afac6d112c20f20fdfeb88849103cb0
GW <kowonp@gmail.com>
2022-05-02 03:35:37 +0900

    build(_app.tsx): set global configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,23 @@
-import "../styles/globals.css";
+import "styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 config.autoAddCss = false;
 
-function MyApp({ Component, pageProps }: AppProps) {
+const fetcher = async (url: string) => {
+  return await (await fetch(url)).json();
+};
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <div>
-      <Component {...pageProps} />
-    </div>
+    <SWRConfig value={{ fetcher }}>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
   );
-}
+};
 
 export default MyApp;
