2e50348038bd6b27ad102df65cbd892294ffbfd1
GW <kowonp@gmail.com>
2022-05-02 03:44:18 +0900

    refactor(prisma.ts): instantiate PrismaClient with Next.js

--- libs/server/prisma.ts
+++ libs/server/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient({ log: ["query"] });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export default prisma;
