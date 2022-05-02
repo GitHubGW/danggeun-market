68c8a83358b748d9d68563e9f7030680e9c96260
GW <kowonp@gmail.com>
2022-05-02 19:40:54 +0900

    feat: products

---
+++ pages/api/products/index.ts
@@ -0,0 +1,17 @@
+import withHandler, { ResponseData } from "libs/server/withHandler";
+import { withSessionRoute } from "libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  try {
+    const foundProducts = await prisma?.product.findMany({});
+    console.log("foundProducts", foundProducts);
+
+    return res.status(200).json({ ok: true, message: "전체 상품 보기에 성공하였습니다.", products: foundProducts });
+  } catch (error) {
+    console.log("products handler error");
+    return res.status(400).json({ ok: false, message: "전체 상품 보기에 실패하였습니다." });
+  }
+};
+
+export default withSessionRoute(withHandler({ methods: ["GET"], handler, isPrivate: false }));
---
+++ pages/products/index.tsx
@@ -0,0 +1,37 @@
+import MainLayout from "components/layouts/main-layout";
+import useSWR from "swr";
+import { CommonResult } from "libs/server/withHandler";
+import { Product } from ".prisma/client";
+import Link from "next/link";
+import ProductItem from "components/items/product-item";
+
+interface ProductsResult extends CommonResult {
+  products?: Product[] | undefined;
+}
+
+const Products = () => {
+  const { data } = useSWR<ProductsResult>(`/api/products`);
+  console.log("data", data);
+
+  return (
+    <MainLayout pageTitle="중고거래" hasFooter={true}>
+      <section>
+        <div>
+          <div className="content py-20">
+            <h2 className="font-medium text-3xl leading-tight text-center">중고거래 인기매물</h2>
+            <div className="grid grid-cols-4 mt-14 gap-x-10 gap-y-12">
+              {data?.products?.map((product: Product) => (
+                <ProductItem key={product.id} {...product} />
+              ))}
+            </div>
+            <Link href="/">
+              <a className="underline mt-14 text-center font-semibold block">더 보기</a>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </MainLayout>
+  );
+};
+
+export default Products;
