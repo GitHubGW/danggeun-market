e980e28c079a41964c72eeaffbcddea6d689a35e
GW <kowonp@gmail.com>
2022-05-06 23:16:37 +0900

    feat: user edit

---
+++ pages/api/users/[username]/edit.ts
@@ -0,0 +1,43 @@
+import withHandler, { ResponseData } from "libs/server/withHandler";
+import { withSessionRoute } from "libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  try {
+    const {
+      body: { file, username, email, phone },
+      session: { loggedInUser },
+    } = req;
+    console.log("file", file);
+
+    let countedUser: number | undefined;
+    const foundUser = await prisma?.user.findUnique({ where: { id: loggedInUser?.id } });
+
+    if (username && username !== foundUser?.username) {
+      countedUser = await prisma?.user.count({ where: { username } });
+      if (countedUser !== 0) {
+        return res.status(400).json({ ok: false, message: "이미 존재하는 유저 이름입니다." });
+      }
+    }
+    if (email && email !== foundUser?.email) {
+      countedUser = await prisma?.user.count({ where: { email } });
+      if (countedUser !== 0) {
+        return res.status(400).json({ ok: false, message: "이미 존재하는 이메일입니다." });
+      }
+    }
+    if (phone && phone !== foundUser?.phone) {
+      countedUser = await prisma?.user.count({ where: { phone } });
+      if (countedUser !== 0) {
+        return res.status(400).json({ ok: false, message: "이미 존재하는 휴대폰 번호입니다." });
+      }
+    }
+
+    await prisma?.user.update({ where: { id: loggedInUser?.id }, data: { username, email, phone } });
+    return res.status(200).json({ ok: true, message: "프로필 수정에 성공하였습니다." });
+  } catch (error) {
+    console.log("user edit handler error", error);
+    return res.status(400).json({ ok: false, message: "프로필 수정에 실패하였습니다." });
+  }
+};
+
+export default withSessionRoute(withHandler({ methods: ["POST"], handler, isPrivate: true }));
--- pages/users/[username]/edit.tsx
+++ pages/users/[username]/edit.tsx
@@ -6,39 +6,97 @@ import PhoneInput from "components/phone-input";
 import FileInput from "components/file-input";
 import { useForm } from "react-hook-form";
 import Input from "components/input";
+import useMe from "libs/client/useMe";
+import { useEffect } from "react";
+import FormError from "components/form-error";
+import useMutation from "libs/client/useMutation";
+import { NextRouter, useRouter } from "next/router";
+import { CommonResult } from "libs/server/withHandler";
+
+interface UserEditFormData {
+  file: FileList;
+  username: string;
+  email: string;
+  phone: string;
+  error?: string;
+}
 
 const UserEdit: NextPage = () => {
-  const { register } = useForm();
+  const me = useMe();
+  const router: NextRouter = useRouter();
+  const [userEditMutation, { data, loading }] = useMutation<CommonResult>(`/api/users/${router.query.username}/edit`);
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm<UserEditFormData>({ mode: "onChange" });
+
+  const onValid = () => {
+    if (loading === true) {
+      return;
+    }
+    const { file, username, email, phone } = getValues();
+    if (email === "" && phone === "") {
+      return setError("error", { message: "이메일 또는 휴대폰 번호를 입력해주세요." });
+    }
+    userEditMutation({ username, email, phone });
+  };
+
+  const onKeyDown = () => {
+    clearErrors("error");
+  };
+
+  useEffect(() => {
+    if (data?.ok === false) {
+      setError("error", { message: data.message });
+    } else if (data?.ok === true) {
+      const { username } = getValues();
+      router.push(`/users/${username}/posts`);
+    }
+  }, [data, setError, getValues, router]);
+
+  useEffect(() => {
+    setValue("username", me?.username || "");
+    setValue("email", me?.email || "");
+    setValue("phone", me?.phone || "");
+  }, [me?.username, me?.email, me?.phone, setValue]);
 
   return (
     <MainLayout pageTitle="프로필 수정" hasFooter={true}>
       <div className="wrapper without-header-footer">
         <div className="content-sub flex items-center">
-          <form className="flex flex-col w-full">
-            <label className="transition-all rounded-full flex justify-center items-center cursor-pointer">
-              <img
-                src={true ? "/images/basic_user.png" : "https://newsimg.sedaily.com/2022/02/21/26288ZY0E1_3.jpg"}
-                alt=""
-                className="border border-gray-200 rounded-full w-56 h-56"
-              />
-              <FileInput register={register("file")} required={false} />
-            </label>
+          <form onSubmit={handleSubmit(onValid)} className="flex flex-col w-full pt-10">
+            <div className="flex justify-center">
+              <label className="transition-all rounded-full flex justify-center items-center cursor-pointer">
+                <img src={me?.avatarUrl ? me?.avatarUrl : "/images/basic_user.png"} alt="" className="border border-gray-200 rounded-full w-56 h-56" />
+                <FileInput register={register("file")} required={false} />
+              </label>
+            </div>
             <label className="mt-4">
               <LabelTitle text="유저 이름" />
-              <Input register={register("username")} type="text" placeholder="유저 이름을 입력해주세요." required={false} />
+              <Input register={register("username")} onKeyDown={onKeyDown} type="text" placeholder="유저 이름을 입력해주세요." required={false} />
             </label>
             <label className="mt-4">
               <LabelTitle text="이메일" />
-              <Input register={register("email")} type="email" placeholder="이메일을 입력해주세요." required={false} />
+              <Input register={register("email")} onKeyDown={onKeyDown} type="email" placeholder="이메일을 입력해주세요." required={false} />
             </label>
             <label className="mt-4 mb-4">
               <LabelTitle text="휴대폰 번호" />
               <div className="relative">
                 <span className="absolute top-1/2 left-2.5 -translate-y-1/2 text-gray-300 select-none">+82</span>
-                {/* <PhoneInput register={register("phone")} required={true} /> */}
+                <PhoneInput register={register("phone")} onKeyDown={onKeyDown} required={false} />
               </div>
             </label>
-            <Button type="submit" text="프로필 업데이트" size="w-full" />
+            {errors.error?.message ? (
+              <div className="mb-2">
+                <FormError text={errors.error?.message || ""} />
+              </div>
+            ) : null}
+            <Button loading={loading} type="submit" text="프로필 업데이트" size="w-full" />
           </form>
         </div>
       </div>
