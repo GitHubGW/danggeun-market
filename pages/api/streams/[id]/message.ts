ecdf2acf8ce2bf583af8fb51079926030a405337
GW <kowonp@gmail.com>
2022-05-07 04:46:55 +0900

    feat: stream message

---
+++ pages/api/streams/[id]/message.ts
@@ -0,0 +1,33 @@
+import withHandler, { ResponseData } from "libs/server/withHandler";
+import { withSessionRoute } from "libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  try {
+    const {
+      body: { text },
+      query: { id },
+      session: { loggedInUser },
+    } = req;
+
+    const foundStream = await prisma?.stream.findUnique({ where: { id: +id } });
+    if (foundStream === null || foundStream === undefined) {
+      return res.status(404).json({ ok: false, message: "존재하지 않는 스트리밍입니다." });
+    }
+
+    await prisma?.streamMessage.create({
+      data: {
+        text,
+        stream: { connect: { id: +id } },
+        user: { connect: { id: loggedInUser?.id } },
+      },
+    });
+
+    return res.status(200).json({ ok: true, message: "스트리밍 메세지 생성에 성공하였습니다." });
+  } catch (error) {
+    console.log("stream detail message add handler error");
+    return res.status(400).json({ ok: false, message: "스트리밍 메세지 생성에 실패하였습니다." });
+  }
+};
+
+export default withSessionRoute(withHandler({ methods: ["POST"], handler, isPrivate: false }));
