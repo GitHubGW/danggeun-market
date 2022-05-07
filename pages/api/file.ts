55e14b1ce2480d0f5e4d2d09c9e8f39b337467cc
GW <kowonp@gmail.com>
2022-05-08 04:55:40 +0900

    feat(file.ts): direct creator upload in cloudflare

---
+++ pages/api/file.ts
@@ -0,0 +1,37 @@
+import withHandler, { ResponseData } from "libs/server/withHandler";
+import { withSessionRoute } from "libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface Response {
+  result: {
+    id: string;
+    uploadURL: string;
+  };
+  result_info: any;
+  success: boolean;
+  errors: any[];
+  messages: any[];
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  try {
+    const cloudflareRequestUrl = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`;
+
+    const response: Response = await (
+      await fetch(cloudflareRequestUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.CLOUDFLARE_IMAGE_API_TOKEN}`,
+        },
+      })
+    ).json();
+
+    return res.status(200).json({ ok: true, message: "Cloudflare Upload URL을 받는데 성공하였습니다.", cloudflareUploadUrl: response.result.uploadURL });
+  } catch (error) {
+    console.log("file handler error");
+    return res.status(400).json({ ok: false, message: "Cloudflare Upload URL을 받는데 실패하였습니다." });
+  }
+};
+
+export default withSessionRoute(withHandler({ methods: ["GET"], handler, isPrivate: true }));
