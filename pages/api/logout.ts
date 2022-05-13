2f8ecb401d8346c0bc67e171ec8e51d6d2ff85e0
GW <kowonp@gmail.com>
2022-05-14 03:47:44 +0900

    feat: logout

---
+++ pages/api/logout.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseData } from "libs/server/withHandler";
+import { withSessionRoute } from "libs/server/withSession";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  try {
+    req.session.loggedInUser = undefined;
+    await req.session.save();
+    return res.status(200).json({ ok: true, message: "로그아웃에 성공하였습니다." });
+  } catch (error) {
+    console.log("logout handler error");
+    return res.status(400).json({ ok: false, message: "로그아웃에 실패하였습니다." });
+  }
+};
+
+export default withSessionRoute(withHandler({ methods: ["POST"], handler, isPrivate: true }));
