eecb65305e4bd3c0a2a3836b17cada05e9dd2f0c
GW <kowonp@gmail.com>
2022-05-14 03:39:19 +0900

    feat: user soldout

---
+++ pages/api/products/[id]/soldout.ts
@@ -0,0 +1,26 @@
+import withHandler, { ResponseData } from "libs/server/withHandler";
+import { withSessionRoute } from "libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  try {
+    const {
+      query: { id },
+      session: { loggedInUser },
+    } = req;
+
+    const countedProduct = await prisma?.product.count({ where: { id: +id, userId: loggedInUser?.id } });
+    if (!countedProduct) {
+      return res.status(404).json({ ok: false, message: "존재하지 않는 상품입니다." });
+    }
+
+    await prisma?.product.update({ where: { id: +id }, data: { isSelling: false } });
+
+    return res.status(200).json({ ok: true, message: "상품 판매 완료에 성공하였습니다." });
+  } catch (error) {
+    console.log("product detail soldout error");
+    return res.status(400).json({ ok: false, message: "상품 판매 완료에 실패하였습니다." });
+  }
+};
+
+export default withSessionRoute(withHandler({ methods: ["POST"], handler, isPrivate: true }));
---
+++ pages/users/[username]/soldout.tsx
@@ -0,0 +1,38 @@
+import { NextPage } from "next";
+import MainLayout from "components/layouts/main-layout";
+import UserLayout from "components/layouts/user-layout";
+import ProductItem from "components/items/product-item";
+import { NextRouter, useRouter } from "next/router";
+import { Product } from ".prisma/client";
+import useSWRInfiniteScroll from "libs/client/useSWRInfiniteScroll";
+
+interface ProductWithUserAndCount extends Product {
+  user: { address: string | null };
+  _count: { productLikes: number };
+}
+
+const UserSoldOut: NextPage = () => {
+  const router: NextRouter = useRouter();
+  const infiniteData = useSWRInfiniteScroll<ProductWithUserAndCount>(router.query.username ? `/api/users/${router.query.username}/soldout` : null);
+
+  return (
+    <MainLayout pageTitle="판매 완료 물품" hasFooter={true}>
+      <UserLayout>
+        {infiniteData?.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            price={product.price}
+            cloudflareImageId={product.cloudflareImageId}
+            user={product.user}
+            _count={product._count}
+            isSelling={product.isSelling}
+          />
+        ))}
+      </UserLayout>
+    </MainLayout>
+  );
+};
+
+export default UserSoldOut;
